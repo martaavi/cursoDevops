117e11e09d9e6ba647d198c7e8a32fd5c555af9e
martaavi <marta.ka@hotmail.com>
2024-12-15 10:46:47 +0100

    Update divide.py

--- app/divide.py
+++ app/divide.py
@@ -4,16 +4,22 @@ import app
 class InvalidPermissions(Exception):
     pass
 
-
 class Calculator:
+    def add(self, x, y):
+        self.check_types(x, y)
+        return x / y  # suma
+        
     def divide(self, x, y):
         self.check_types(x, y)
         if y == 0:
-            raise TypeError("error HTTP 406")
-
+            raise ValueError("ERROR HTTP 406")  # Cambié TypeError a ValueError
         return x / y
 
-if __name__ == "__main__":  # pragma: no cover
+    def check_types(self, x, y):
+        if not isinstance(x, (int, float)) or not isinstance(y, (int, float)):
+            raise TypeError("Parameters must be numbers")
+
+if __name__ == "__main__":  # testeo que sin las barras bajas no de error
     calc = Calculator()
-    result = calc.add(2, 2)
+    result = calc.add(4, 0)
     print(result)
