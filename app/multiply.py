3b8df7d9b16c362b3393ea819289b4224388e76f
martaavi <marta.ka@hotmail.com>
2024-12-15 10:05:22 +0100

    Create multiply.py

---
+++ app/multiply.py
@@ -0,0 +1,21 @@
+import app
+
+
+class InvalidPermissions(Exception):
+    pass
+
+
+class Calculator:
+    def multiply(self, x, y):
+        self.check_types(x, y)
+        return x * y
+
+    def check_types(self, x, y):
+        if not isinstance(x, (int, float)) or                                                not isinstance(y, (int, float)):
+            raise TypeError("Parameters must be numbers")
+
+
+if __name__ == "__main__":  # pragma: no cover
+    calc = Calculator()
+    result = calc.add(2, 2)
+    print(result)
